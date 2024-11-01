6138ee6bc1e862dc895224414f40c97d07e8fa74
Happily44 <sierra3244@icloud.com>
2024-11-01 23:33:12 +0900

    Update script.js

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ function handleNext() {
             document.getElementById('googleMapSection').style.display = 'block';
         } else {
             // それ以外の評価ではコメント入力画面に遷移
-            window.location.href = "http://127.0.0.1:5500/feedback-form.html"; // コメントページのURL
+            window.location.href = "http://localhost:5500/feedback-form.html"; // コメントページのURL
         }
     } else {
         alert('評価を選択してください。');
