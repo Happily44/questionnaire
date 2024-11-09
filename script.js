4c9f1dd36d66b278aaa686abd7ad10a95e6b543e
Happily44 <sierra3244@icloud.com>
2024-11-09 16:27:31 +0900

    Update script.js

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ function handleNext() {
             document.getElementById('googleMapSection').style.display = 'block';
         } else {
             // それ以外の評価の場合、フォームを送信してコメントページにリダイレクト
-            document.getElementById('feedbackForm').submit();
+           window.location.href = "/feedback-form.html"
         }
     } else {
         alert('評価を選択してください。');
